d8641635c5ec2feed795484868e12013de59a116
lichenggang <lichenggang@uniontech.com>
2026-04-03 16:25:44 +0800

    fpcmoc: Add support for 0x10A5:0x9B24
    Add the new FPC fingerprint device (0x10A5:0x9B24) to the driver and
    autosuspend hwdb.
    
    Signed-off-by: lichenggang <lichenggang@uniontech.com>

--- libfprint/drivers/fpcmoc/fpc.c
+++ libfprint/drivers/fpcmoc/fpc.c
@@ -72,6 +72,7 @@ static const FpIdEntry id_table[] = {
   { .vid = 0x10A5,  .pid = 0x9524,  },
   { .vid = 0x10A5,  .pid = 0x9544,  },
   { .vid = 0x10A5,  .pid = 0xC844,  },
+  { .vid = 0x10A5,  .pid = 0x9B24,  },
   /* terminating entry */
   { .vid = 0,  .pid = 0,  .driver_data = 0 },
 };
