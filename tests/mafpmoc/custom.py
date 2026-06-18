4f743766a54fcdac36ba333ff9e2d409b248c4b5
Marco Trevisan (Treviño) <mail@3v1n0.net>
2026-06-18 14:59:55 +0200

    mafpmoc/custom: Improve test script checking for identified match

--- tests/mafpmoc/custom.py
+++ tests/mafpmoc/custom.py
@@ -40,16 +40,18 @@ def enroll_progress(*args):
     print('enroll progress: ' + str(args))
 
 def identify_done(dev, res):
-    global identified
+    global identified, identify_match
+
     identified = True
     identify_match, identify_print = dev.identify_finish(res)
-    print('indentification_done: ', identify_match, identify_print)
+    print('indentification_done. match:', identify_match.get_description(),
+          'print:', identify_print.get_description())
     assert identify_match.equal(identify_print)
 
 # List, enroll, list, verify, identify, delete
 print("enrolling")
 assert d.get_finger_status() == FPrint.FingerStatusFlags.NONE
-p = d.enroll_sync(template, None, enroll_progress, None)
+enrolled = d.enroll_sync(template, None, enroll_progress, None)
 assert d.get_finger_status() == FPrint.FingerStatusFlags.NONE
 print("enroll done")
 
@@ -57,13 +59,12 @@ print("listing")
 stored = d.list_prints_sync()
 print("listing done")
 assert len(stored) == 1
-assert stored[0].equal(p)
+assert stored[0].equal(enrolled)
 print("verifying")
 assert d.get_finger_status() == FPrint.FingerStatusFlags.NONE
-verify_res, verify_print = d.verify_sync(p)
+verify_res, verify_print = d.verify_sync(enrolled)
 assert d.get_finger_status() == FPrint.FingerStatusFlags.NONE
 print("verify done")
-del p
 assert verify_res == True
 
 identified = False
@@ -74,15 +75,20 @@ for p in stored:
 del stored
 
 print('async identifying')
+identify_match = None
 d.identify(deserialized_prints, callback=identify_done)
 del deserialized_prints
 
 while not identified:
     ctx.iteration(True)
 
+assert identify_match.equal(enrolled)
+
 print("deleting")
-d.delete_print_sync(p)
+d.delete_print_sync(enrolled)
 print("delete done")
+
+del enrolled
 
 d.close_sync()
 
